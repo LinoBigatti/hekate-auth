6a495ee086c9962cecf10c6c2be06f69a098733c
lino <Lino Bigatti>
2020-05-29 18:12:45 -0300

    Added comments to everything. Updated ARCH.md.

--- ARCH.md
+++ ARCH.md
@@ -1,31 +1,47 @@
 # Architecture description
 
 ## User authentication
+
 1. User inputs username and password.
 2. Password is hashed via sha256.
 3. Frontend relays them to the auth script.
 4. Auth script reads from hashes.json file.
 5. It tries to get the entry which id corresponds to the username.
-5.1 If nothing is found, return 1.
-5.2 Else, proceed to step 6.
+	1. If nothing is found, return 1.
+	2. Else, proceed to step 6.
 6. Compare password's argon2 hash stored corresponds with the password hash sent.
-6.1 If it does not match, return 1.
-6.2 Else, proceed to step 7.
+	1. If it does not match, return 1.
+	2. Else, proceed to step 7.
 7. Generate an argon2 hash based on nanoseconds since epoch to use as a session token.
 8. Return the token.
 
 ## Signing up
+
 1. User inputs username and password.
 2. Password gets hashed via sha256
 3. Frontend relays them to the auth script.
 4. Auth script checks if the username is taken.
-4.1 If it is, return 1.
-4.2 Else, proceed to step 4.
+	1. If it is, return 2.
+	2. Else, proceed to step 4.
 5. Auth script checks if the password is in the most common passwords.
-5.1 If thats true, return 2.
-5.2 Else, continue to step 6
+	1. If thats true, return 3.
+	2. Else, continue to step 6
 6. Auth script hashes password and appends username and hashed password to hashes.json.
 7. Return 0.
 
 ## Server
-todo :kekw:
+
+1. Server receives the request.
+	1. If its GET, go to step 3.
+	2. If its POST, continue to step 2.
+2. Check for operation number.
+	1. If its 0, send name and password and go to user authentication.
+		1. If it returns 1, the password or the username are incorrect. Send a 403 error, accompanied of a description.
+		2. Else, send a 200 and return the token.
+	2. If its 1, send name and password and go to sign up.
+		1. If it returns 0, it was successfull. Send a 200 code and inform the user.
+		2. If it returns 2, the username is taken. Send a 403 error, accompanied of a description.
+		3. If it returns 3, the password is too common. Send a 403 error together with a description.
+	3. If its nor 0 or 1, send a 501 error.
+	4. If its not a number, send a 400 error and go to step 3.
+3. Display the help message.
--- src/main.py
+++ src/main.py
@@ -7,55 +7,58 @@ import json
 import sys
 import hashlib
 
+common = {}     #Load the 10.000 most common passwords list (Hashed with sha256)
+with open('common.json', 'r') as f:
+    common = json.loads(f.read())
+
+#Sign in
 def auth(name : str, password : str):
-    data = {}
+    data = {}   #Load the user data
     with open('hashes.json', 'r') as f:
         data = json.loads(f.read())
 
-    try:
+    try:    #Try to get the data for a given name
         passwordHash = data[name]
 
         hasher = PasswordHasher()
         try:
-            hasher.verify(passwordHash, password)
-            token = hasher.hash(str(nanos()))
-            return token
-        except VerifyMismatchError:
-            return 1
-    except KeyError:
-        return 1
+            hasher.verify(passwordHash, password)   #Verify the password
 
+            token = hasher.hash(str(nanos()))[30:]  #Generate a unique token
+            return token    #Send the token
+        except VerifyMismatchError: #Incorrect password
+            return 1    #Incorrect user or password
+    except KeyError:    #Incorrect user
+        return 1        #Incorrect user or password
+
+#Sign up
 def signUp(name, password):
-    data = {}
+    data = {}   #Load the user data
     with open('hashes.json', 'r') as f:
         data = json.loads(f.read())
 
-    try:
+    try:    #See if the username exists
         exists_ = data[name]
-        return 2
+        return 2    #Username taken
     except KeyError:
         pass
-
-    common = {}
-    with open('common.json', 'r') as f:
-        common = json.loads(f.read())
     
-    for pwd in common:
-        if pwd == password:
-            return 3
+    for pwd in common:  #Check every common password
+        if pwd == password: #If the password inputted is common, don't accept it
+            return 3    #Password is too common
 
     hasher = PasswordHasher()
-    data[name] = hasher.hash(password)
+    data[name] = hasher.hash(password)  #Rehash the password using argon2
 
-    with open('hashes.json', 'w') as f:
+    with open('hashes.json', 'w') as f: #Save the list
         json.dump(data, f)
 
-    return 0
+    return 0    #Success
 
-if __name__ == '__main__':
+if __name__ == '__main__':  #Handle being called directly
     sha256 = hashlib.sha256()
     sha256.update(bytes(sys.argv[2], encoding='utf8'))
-    if sys.argv[3] == '0':
+    if sys.argv[3] == '0':  #Sign in
         print(str(auth(sys.argv[1], sha256.hexdigest())))
-    else:
+    else:                   #Sign up
         print(str(signUp(sys.argv[1], sha256.hexdigest())))
